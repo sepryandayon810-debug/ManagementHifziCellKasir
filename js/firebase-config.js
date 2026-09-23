e974c66e3dd8285fc9432ad07a842e30c7d07781
sepryandayon810-debug <sepryandayon810@gmail.com>
2026-09-23 16:49:22 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,18 +1,32 @@
 // Konfigurasi Firebase WebPOS (Firestore)
-const firebaseConfig = {
-  apiKey: "AIzaSyCKU4LAF4sEaIMvsvNDTf_kU-7JmprsdMM",
-  authDomain: "managementhifzicell.firebaseapp.com",
-  projectId: "managementhifzicell",
-  storageBucket: "managementhifzicell.firebasestorage.app",
-  messagingSenderId: "275855765943",
-  appId: "1:275855765943:web:93ed6975a3417fb3c73832"
-};
+if (!window.firebaseConfigInitialized) {
+  window.firebaseConfigInitialized = true;
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+  const firebaseConfig = {
+    apiKey: "AIzaSyCKU4LAF4sEaIMvsvNDTf_kU-7JmprsdMM",
+    authDomain: "managementhifzicell.firebaseapp.com",
+    projectId: "managementhifzicell",
+    storageBucket: "managementhifzicell.firebasestorage.app",
+    messagingSenderId: "275855765943",
+    appId: "1:275855765943:web:93ed6975a3417fb3c73832"
+  };
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  // Layanan Firebase Global
+  window.auth = firebase.auth();
+  window.db = firebase.firestore();
+  
+  // Storage pengaman jika diperlukan
+  try {
+    window.storage = firebase.storage();
+  } catch(e) {
+    window.storage = null;
+  }
 }
 
-// Layanan Firebase Global
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage();
+const auth = window.auth;
+const db = window.db;
+const storage = window.storage;
